32fac9575e96cc07fb35b75ea5761bbe72b8ee50
davidkron <david_jensen3@hotmail.com>
2015-12-08 23:08:15 +0100

    Cleaned up autogenerated tests

--- HotelSystem.tests/src/SoftwareModel/BusinessLogic/tests/RoomsFinderTest.java
+++ HotelSystem.tests/src/SoftwareModel/BusinessLogic/tests/RoomsFinderTest.java
@@ -2,33 +2,17 @@
  */
 package SoftwareModel.BusinessLogic.tests;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EOperation;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreEList;
-
-import SoftwareModel.BusinessLogic.BusinessLogicFactory;
-import SoftwareModel.BusinessLogic.Rooms;
 import SoftwareModel.BusinessLogic.impl.AvailibleRoomFinderImpl;
 import SoftwareModel.DataAccess.RoomBookingsRepository;
 import SoftwareModel.DataAccess.RoomRepository;
-import SoftwareModel.DataAccess.impl.RoomRepositoryImpl;
 import SoftwareModel.DomainEntities.Availability;
 import SoftwareModel.DomainEntities.BedType;
 import SoftwareModel.DomainEntities.Room;
 import SoftwareModel.DomainEntities.RoomBooking;
-import SoftwareModel.DomainEntities.RoomResponsible;
 import SoftwareModel.DomainEntities.RoomType;
 import SoftwareModel.DomainEntities.impl.RoomBookingImpl;
 import SoftwareModel.DomainEntities.impl.RoomImpl;
@@ -37,43 +21,16 @@ import junit.framework.TestCase;
 import static org.mockito.Mockito.*;
 import junit.textui.TestRunner;
 
-/**
- * <!-- begin-user-doc -->
- * A test case for the model object '<em><b>Rooms</b></em>'.
- * <!-- end-user-doc -->
- * <p>
- * The following operations are tested:
- * <ul>
- *   <li>{@link SoftwareModel.BusinessLogic.Rooms#checkOut(SoftwareModel.DomainEntities.RoomBooking) <em>Check Out</em>}</li>
- *   <li>{@link SoftwareModel.BusinessLogic.Rooms#checkIn(SoftwareModel.DomainEntities.RoomBooking) <em>Check In</em>}</li>
- *   <li>{@link SoftwareModel.BusinessLogic.Rooms#getBooking(int) <em>Get Booking</em>}</li>
- *   <li>{@link SoftwareModel.BusinessLogic.Rooms#availibleRoomTypes(int, int, java.util.Date, java.util.Date) <em>Availible Room Types</em>}</li>
- *   <li>{@link SoftwareModel.BusinessLogic.Rooms#getBooking(java.lang.String) <em>Get Booking</em>}</li>
- * </ul>
- * </p>
- * @generated
- */
-public class RoomsTest extends TestCase {
+public class RoomsFinderTest extends TestCase {
 
 	RoomRepository fakeRepo;
 	RoomBookingsRepository fakeBookings;
 	AvailibleRoomFinderImpl roomFinder;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
 	public static void main(String[] args) {
-		TestRunner.run(RoomsTest.class);
+		TestRunner.run(RoomsFinderTest.class);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#setUp()
-	 * @generated
-	 */
 	@Override
 	protected void setUp() throws Exception {
 		fakeRepo = mock(RoomRepository.class);
@@ -81,12 +38,6 @@ public class RoomsTest extends TestCase {
 		roomFinder = new AvailibleRoomFinderImpl(fakeBookings,fakeRepo);
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @see junit.framework.TestCase#tearDown()
-	 * @generated
-	 */
 	@Override
 	protected void tearDown() throws Exception {
 		roomFinder = null;
@@ -159,4 +110,4 @@ public class RoomsTest extends TestCase {
 		assertEquals(0, roomtypes.size());
 	}
 
-} //RoomsTest
+}
